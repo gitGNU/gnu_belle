c4307fd066d6837d59c841c990a3e5f29fb8ccec
freemind <freemind@lavabit.com>
2013-12-07 18:40:58 +0000

    Added init file.

---
+++ Engine/init.js
@@ -0,0 +1,41 @@
+belle = {};
+belle.game = {
+    "resources" : {},
+    "actions": [],
+    "customFonts" : [],
+    "currentScene" : null,
+    "nextScene" : 0,
+    "scenes" : [],
+    "ready" : false,
+    "nextAction" : 0,
+    "width": 640,
+    "height" : 480,
+    "currentWidth": 640,
+    "currentHeight": 480,
+    "variables" : {},
+    "end" : false,
+    "textSpeed" : 10,
+    "textDelay" : 100,
+    "filename" : "game.json",
+    "directory" : "",
+    "states" : {},
+    "data" : null,
+    "paused": false
+};
+
+belle.game.pauseScreen = {
+    "currentScene" : null,
+    "nextScene" : 1,
+    "scenes" : [],
+    "actions": [],
+    "nextAction" : 0,
+    "currentAction": null,
+    "states" : {},
+    "end": false
+};
+
+belle.actions = {};
+belle.objects = {};
+belle.display = {};
+belle.utils = {};
+belle.stateMachine = {};
