2602cc1b575746f5ae36f3efb1fc7fe815e618cc
freemind <freemind@live.com.pt>
2014-02-16 02:42:51 +0000

    Moved extend functions for Array and String objects from utils.js to init.js.

--- Engine/init.js
+++ Engine/init.js
@@ -39,3 +39,64 @@ belle.objects = {};
 belle.display = {};
 belle.utils = {};
 belle.stateMachine = {};
+
+//Extend Array and String objects
+if (typeof String.prototype.startsWith != 'function') 
+{
+  String.prototype.startsWith = function (str) {
+    return this.slice(0, str.length) == str;
+  };
+}
+
+if (typeof Array.prototype.indexOf != 'function')
+{
+  Array.prototype.indexOf = function(value, start)
+  {
+    if (! start)
+        start = 0;
+    
+    for (var i=start; i < this.length; i++)
+        if (this[i] === value)
+            return i;
+   
+    return -1;
+  };
+}
+
+if (typeof String.prototype.contains != 'function') 
+{
+    String.prototype.contains = function(text) { 
+        return this.indexOf(text) != -1; 
+    };
+}
+
+if (typeof String.prototype.trim != 'function') 
+{
+    String.prototype.trim =  function(){
+        return this.replace(/^\s+|\s+$/g, '');
+        
+    };
+}
+
+if (typeof String.prototype.isDigit != 'function') 
+{
+    String.prototype.isDigit = function(text) { 
+        text = text.trim();
+        return ! isNaN(text); 
+    };
+}
+
+if (typeof String.prototype.hasBreakPointAt != 'function') 
+{
+    String.prototype.hasBreakPointAt = function(i) { 
+        return this[i] == " " || this[i] == "\n"; 
+    };
+}
+
+if (typeof Array.prototype.contains != 'function') 
+{
+    Array.prototype.contains = function(text) { 
+        return this.indexOf(text) != -1; 
+    };
+}
+
--- Engine/utils.js
+++ Engine/utils.js
@@ -13,71 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-//New methods for Javascript's Array and String objects
-if (typeof String.prototype.startsWith != 'function') 
-{
-  String.prototype.startsWith = function (str) {
-    return this.slice(0, str.length) == str;
-  };
-}
-
-if (typeof Array.prototype.indexOf != 'function')
-{
-  Array.prototype.indexOf = function(value, start)
-  {
-    if (! start)
-        start = 0;
-    
-    for (var i=start; i < this.length; i++)
-        if (this[i] === value)
-            return i;
-   
-    return -1;
-  };
-}
-
-if (typeof String.prototype.contains != 'function') 
-{
-    String.prototype.contains = function(text) { 
-        return this.indexOf(text) != -1; 
-    };
-}
-
-if (typeof String.prototype.trim != 'function') 
-{
-    String.prototype.trim =  function(){
-        return this.replace(/^\s+|\s+$/g, '');
-        
-    };
-}
-
-if (typeof String.prototype.isDigit != 'function') 
-{
-    String.prototype.isDigit = function(text) { 
-        text = text.trim();
-        return ! isNaN(text); 
-    };
-}
-
-if (typeof String.prototype.hasBreakPointAt != 'function') 
-{
-    String.prototype.hasBreakPointAt = function(i) { 
-        return this[i] == " " || this[i] == "\n"; 
-    };
-}
-
-if (typeof Array.prototype.contains != 'function') 
-{
-    Array.prototype.contains = function(text) { 
-        return this.indexOf(text) != -1; 
-    };
-}
-
-
-var belle = belle || {};
-belle.utils = {};
-
+ 
 (function(utils) {
 
 var dummy = null;
